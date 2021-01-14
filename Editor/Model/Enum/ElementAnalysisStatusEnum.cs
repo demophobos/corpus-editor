e52074ddbb921704ca5f90efecbe883577ec71c0
agricola <demophobos@gmail.com>
2021-01-14 12:49:31 +0300

    removing red

--- Editor/Model/Enum/ElementAnalysisStatusEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model.Enum
-{
-    public enum ElementAnalysisStatusEnum
-    {
-        Proposed = 0,
-        Accepted = 1,
-        Rejected = 2
-    }
-}
--- Editor/Model/Enum/TextAnalysisStatusEnum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model.Enum
-{
-    public enum TextAnalysisStatusEnum
-    {
-        Progress,
-        Done
-    }
-}
