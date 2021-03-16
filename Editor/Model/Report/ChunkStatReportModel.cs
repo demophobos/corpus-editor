4151a92e6976b88c481630558b3e6ebb675687f7
agricola <demophobos@gmail.com>
2021-03-16 12:25:58 +0300

    reports

---
+++ Editor/Model/Report/ChunkStatReportModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Report
+{
+    public class ChunkStatReportModel
+    {
+        public int Done { get; set; }
+        public int InProgress { get; set; }
+        public int Total
+        {
+            get
+            {
+                return Done + InProgress;
+            }
+        }
+    }
+}
---
+++ Editor/Model/Report/ElementStatReportModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Report
+{
+    public class ElementStatReportModel
+    {
+        public int Resolved { get; set; }
+        public int Unresolved { get; set; }
+        public int Total
+        {
+            get
+            {
+                return Resolved + Unresolved;
+            }
+        }
+    }
+}
